55c5e97d5f69533cd0305115010e8a3889f35c50
shihaim <55915214+shihaim@users.noreply.github.com>
2022-03-14 11:48:37 +0900

    스프링 시큐리티와 OAuth2.0으로 구글 로그인 기능 구현

--- src/main/java/springboot/web/IndexController.java
+++ src/main/java/springboot/web/IndexController.java
@@ -5,18 +5,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import springboot.config.auth.dto.SessionUser;
 import springboot.service.posts.PostsService;
 import springboot.web.dto.PostsResponseDto;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null) {
+            model.addAttribute("userNames", user.getName());
+        }
+
         return "index";
     }
 
